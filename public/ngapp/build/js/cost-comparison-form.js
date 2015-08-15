ad8c43e52c77c2df9c00d528917cece2494e5503
otakisan <takashi@aquamarine.local>
2015-08-15 21:08:35 +0900

    Babelを導入

--- public/ngapp/build/js/cost-comparison-form.js
+++ public/ngapp/build/js/cost-comparison-form.js
@@ -1,6 +1,7 @@
+'use strict';
+
 (function () {
-	angular.module('costComparisonFormApp', [])
-		.directive('costComparisonForm', function () {
+	angular.module('costComparisonFormApp', []).directive('costComparisonForm', function () {
 		// templateUrlは、ディレクティブを呼び出しているページからの相対パス
 		// ルートからの相対パスにして、どこからでも使えるようにする
 		return {
@@ -9,7 +10,7 @@
 			controller: ['$http', function ($http) {
 				// ケアレスミスを防ぐため、最初にthisを移し替えることをルールとしたほうがいいのかも
 				var thisController = this;
-			
+
 				// ユーザーをひも付ける情報は、railsのidとアプリのidとどちらがよいか
 				// データベースの移行を考えると、連番よりも固有のアカウントIDのほうがよさそうに感じる
 				// API越しに関連付ける際、デフォルトのUser.idではなく、User.user_idとひも付ける
@@ -20,15 +21,15 @@
 				thisController.userId = "";
 				thisController.waiting = false;
 				thisController.csvUrl = "";
-				thisController.quotationsTerm = {start: "", end: ""};
-			
+				thisController.quotationsTerm = { start: "", end: "" };
+
 				// プロジェクトの種類を取得
 				$http.get('/quotations/projects/index.json').success(function (data) {
 					thisController.projects = data;
 					thisController.selectedProject = thisController.projects[0].project_name;
 					thisController.changedSelectedProject();
 				});
-			
+
 				// プロパティもメソッドも全部定義するから、中身が膨れやすい
 				this.submitForm = function () {
 					thisController.pageTitle = "取得中... : ";
@@ -36,14 +37,12 @@
 					// データを取得
 					thisController.waiting = true;
 					var project_name_encoded = encodeURIComponent(thisController.selectedProject);
-					$http.get("/cost_comparisons/" + project_name_encoded + ".json")
-						.success(function (data, status, headers, config) {
+					$http.get("/cost_comparisons/" + project_name_encoded + ".json").success(function (data, status, headers, config) {
 						// タイトルを元に戻し、データを保持
 						thisController.pageTitle = thisController.pageTitleDefault;
 						thisController.costComparisonResult = data;
 						thisController.waiting = false;
-					})
-						.error(function (data, status, headers, config) {
+					}).error(function (data, status, headers, config) {
 						thisController.pageTitle = "取得失敗";
 						thisController.costComparisonResult = data;
 						thisController.waiting = false;
@@ -51,48 +50,49 @@
 				};
 
 				// TODO: 画面のラベルに表示する文字列はコントローラーの責務でないように思えるが、一旦実装する
-				this.getQuotationTermText = function() {
+				this.getQuotationTermText = function () {
 					return this.getTermText(this.costComparisonResult.quotations);
 				};
 
-				this.getFactTermText = function() {
+				this.getFactTermText = function () {
 					return this.getTermText(this.costComparisonResult.facts);
 				};
 
-				this.getTermText = function(dataArray) {
+				this.getTermText = function (dataArray) {
 					var quotationTermText = "";
 					if (dataArray && dataArray.length > 0) {
 						// 分単位で表示（対応ブラウザのみ）
 						var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-						quotationTermText = new Date(dataArray[0].updated_at).toLocaleString('ja-JP', options).replace("JST", "") + " 〜 "
-						+ new Date(dataArray[dataArray.length - 1].updated_at).toLocaleString('ja-JP', options).replace("JST", "");
+						quotationTermText = new Date(dataArray[0].updated_at).toLocaleString('ja-JP', options).replace("JST", "") + " 〜 " + new Date(dataArray[dataArray.length - 1].updated_at).toLocaleString('ja-JP', options).replace("JST", "");
 					}
-					
+
 					return quotationTermText;
 				};
 
-				this.getProjectNameText = function() {
+				this.getProjectNameText = function () {
 					var dataArray = this.costComparisonResult.quotations || this.costComparisonResult.facts;
 					return dataArray && dataArray.length > 0 ? dataArray[0].project_name : "";
 				};
 
-				this.getTotalManDayText = function() {
+				this.getTotalManDayText = function () {
 					var mandayQ = this.getTotalManDayOfQuotations();
 					var mandayF = this.getTotalManDayOfFacts();
-					return !mandayQ && !mandayF ? "" : `見積：${mandayQ}／ 実績：${mandayF}`;
+					return !mandayQ && !mandayF ? "" : '見積：' + mandayQ + '\b／ 実績：' + mandayF;
 				};
 
-				this.getTotalManDayOfQuotations = function() {
+				this.getTotalManDayOfQuotations = function () {
 					return this.getTotalManDay(this.costComparisonResult.quotations);
 				};
 
-				this.getTotalManDayOfFacts = function() {
+				this.getTotalManDayOfFacts = function () {
 					return this.getTotalManDay(this.costComparisonResult.facts);
 				};
 
-				this.getTotalManDay = function(array) {
+				this.getTotalManDay = function (array) {
 					// TODO: 小数点の扱い 見た目上の合計と差異が出る 12.9 <=> 12.89999...
-					return !array ? "" : array.reduce( (prevValue, currentValue) => prevValue + currentValue.man_day, 0);
+					return !array ? "" : array.reduce(function (prevValue, currentValue) {
+						return prevValue + currentValue.man_day;
+					}, 0);
 				};
 
 				this.getPostData = function () {
@@ -110,28 +110,26 @@
 
 					return postParam;
 				};
-				
+
 				this.downloadCsv = function () {
 					thisController.pageTitle = "CSV取得中... : ";
-				
+
 					// 実績比較表取得のためのデータを取得
 					//var postData = thisController.getPostData();
-				
+
 					// 取得
 					// TODO:どういうリクエストを送ればいい？
 					thisController.waiting = true;
 					var project_name_encoded = encodeURIComponent(thisController.selectedProject);
-					$http.get("/cost_comparisons/download/" + project_name_encoded + ".csv")
-						.success(function (data, status, headers, config) {
+					$http.get("/cost_comparisons/download/" + project_name_encoded + ".csv").success(function (data, status, headers, config) {
 						thisController.pageTitle = "取得完了";
 						thisController.waiting = false;
-					})
-						.error(function (data, status, headers, config) {
+					}).error(function (data, status, headers, config) {
 						thisController.pageTitle = "取得失敗";
 						thisController.waiting = false;
 					});
 				};
-				
+
 				this.changedSelectedProject = function () {
 					var project_name_encoded = encodeURIComponent(thisController.selectedProject);
 					thisController.csvUrl = "/cost_comparisons/download/" + project_name_encoded + ".csv";
@@ -141,3 +139,4 @@
 		};
 	});
 })();
+//# sourceMappingURL=cost-comparison-form.js.map
